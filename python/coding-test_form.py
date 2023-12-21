573399f82dcbaa921be0a76584d5237fce8bb8d0
KChanho <94108712+KChanho@users.noreply.github.com>
2023-12-21 20:45:50 +0900

    Add files via upload

---
+++ python/coding-test_form.py
@@ -0,0 +1,23 @@
+"""
+url
+ - []
+
+
+"""
+
+# 
+
+import sys; input = sys.stdin.readline
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    # input
+    input()
+    # solution
+    solution()
+    # print
+    print()
+
+# 
